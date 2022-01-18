40711d3bf12782cdc7f6371d76ce4249800f1278
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-01-18 18:36:38 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,17 @@ function runScript() {
  if (!div) return alert('Something went wrong :/');
  var date = new Date();
  var times = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
- var dl_link = 'DOWNLOAD_LINK';
- div.innerHTML = `<h3>Server Bot sedang Online!<br>${times}<br><br><br>Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a><br>Download <a href="${dl_link}" target="_blank">AutoResponder (MOD)</a></h3>`;
+ var html = `
+<h2 class="title">Server Bot sedang Online!</h2>
+<h3 class="time">${times}</h3>
+<br><br>
+<h3>Cara Menyambungkan ke Server Bot</h3>
+<h4>ScreenShot: <a href="https://i.ibb.co/ZYHmL5h/Long-Screenshot-20211021-120906.png" target="_blank">Klik Disini</a></h4>
+<h4>Video Tutorial: <a href="https://f.top4top.io/m_2209cxa6n0.mp4" target="_blank">Klik Disini</a></h4>
+<br>
+<h4 class="contact">Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a></h4>
+<h4 class="download">Download <a href="DOWNLOAD_LINK" target="_blank">AutoResponder (MOD)</a></h4>
+`.trim();
+ div.innerHTML = html;
  window.setTimeout('runScript();', 1000);
 }
