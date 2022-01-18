74d59d82540f3c48b20146aaeb4dd69db61d1cfb
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-01-18 15:17:19 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 window.setTimeout('runScript();', Math.floor(Math.random() * 5000));
 
 function runScript() {
-var div = document.querySelector('div[class=content]');
-var date = new Date();
-var times = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
-div.innerHTML = '<h3>Server Bot sedang Online!<br>' + times + '<br><br><br>Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a><br>Download <a href="DOWNLOAD_LINK" target="_blank">AutoResponder</a>';
-window.setTimeout('runScript();', 1000);
+ var div = document.querySelector('div[class=content]');
+ if (!div) return alert('Something went wrong :/');
+ var date = new Date();
+ var times = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
+ var dl_link = 'DOWNLOAD_LINK';
+ div.innerHTML = `<h3>Server Bot sedang Online!<br>${times}<br><br><br>Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a><br>Download <a href="${dl_link}" target="_blank">AutoResponder</a></h3>`;
+ window.setTimeout('runScript();', 1000);
 }
