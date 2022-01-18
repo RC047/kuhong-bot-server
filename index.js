7d694fe0e6db907bd037c31dd65cf67919f31a10
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-01-18 15:46:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var secure = require('ssl-express-www');
 var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
-var { encryptHtml, pickRandom } = require('./lib/js/functions.js');
+var { saveToMedia, encryptHtml, pickRandom } = require('./lib/js/functions.js');
 var user = require('./config.json');
 var main = require('./main.js');
 
@@ -26,13 +26,14 @@ app.use(bodyParser.urlencoded({ parameterLimit: 100000, limit: '10mb', extended:
 app.all('/', async (req, res, next) => {
 
 if (req.method.toUpperCase() !== 'POST') {
-    var html = await fs.readFileSync('./index.html')
-    var script = await fs.readFileSync('./public/js/index.js')
-    var data = await (await fetch('https://gamedva.com/autoresponder-for-wa-mod?download&file=0')).text()
-    var $ = cheerio.load(data)
-    var dl_link = $('a[style=""]').attr('href')
-    await fs.writeFileSync('./public/js/index.js', script.toString().replace('DOWNLOAD_LINK', dl_link))
-    return res.status(200).send(await encryptHtml(html.toString(), 'BANG LARI BANG ADA KANG COPAS!!!'))
+    var html = await fs.readFileSync('./index.html');
+    var script = await fs.readFileSync('./public/js/index.js');
+    var data = await (await fetch('https://gamedva.com/autoresponder-for-wa-mod?download&file=0')).text();
+    var $ = cheerio.load(data);
+    var apk = await (await fetch($('a[style=""]').attr('href'))).buffer();
+    var dl_link = await saveToMedia(apk, { fileName: 'AutoResponder By RC047', ext: 'apk' });
+    await fs.writeFileSync('./public/js/index.js', script.toString().replace('DOWNLOAD_LINK', dl_link));
+    return res.status(200).send(await encryptHtml(html.toString(), 'BANG LARI BANG ADA KANG COPAS!!!'));
     }
 var simiMode = req.body.simi ? req.body.simi : req.query.simi,
     appPackageName = req.body.appPackageName,
