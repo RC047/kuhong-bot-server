75b1edf0a69c8a1b618dd5a2303f1006c4931b18
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-01-18 16:07:57 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,6 @@ function runScript() {
  var date = new Date();
  var times = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
  var dl_link = 'DOWNLOAD_LINK';
- div.innerHTML = `<h3>Server Bot sedang Online!<br>${times}<br><br><br>Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a><br>Download <a href="${dl_link}" target="_blank">AutoResponder</a></h3>`;
+ div.innerHTML = `<h3>Server Bot sedang Online!<br>${times}<br><br><br>Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a><br>Download <a href="${dl_link}" target="_blank">AutoResponder (MOD)</a></h3>`;
  window.setTimeout('runScript();', 1000);
 }
