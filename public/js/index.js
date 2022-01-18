c98602a289b0c6891cba6c1befcf4a2c9287c48a
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-01-18 19:29:04 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,48 @@
 window.setTimeout('runScript();', Math.floor(Math.random() * 5000));
 
 function runScript() {
+
+getBattery(battery => {
  var div = document.querySelector('div[class=content]');
  if (!div) return alert('Something went wrong :/');
  var date = new Date();
- var times = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
  var html = `
-<h2 class="title">Server Bot sedang Online!</h2>
-<h3 class="time">${times}</h3>
-<br><br>
-<h3>Cara Menyambungkan ke Server Bot</h3>
-<h4>ScreenShot: <a href="https://i.ibb.co/ZYHmL5h/Long-Screenshot-20211021-120906.png" target="_blank">Klik Disini</a></h4>
-<h4>Video Tutorial: <a href="https://f.top4top.io/m_2209cxa6n0.mp4" target="_blank">Klik Disini</a></h4>
+<h2 class="title-1">Server Bot sedang Online!</h2>
+<h4>
+Time: ${[date.getHours(), date.getMinutes(), date.getSeconds()].join(':')}
 <br>
-<h4 class="contact">Chat dengan <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Bot</a></h4>
-<h4 class="download">Download <a href="DOWNLOAD_LINK" target="_blank">AutoResponder (MOD)</a></h4>
+Power: ${battery}%
+<br>
+User Agent: ${navigator.userAgent}
+</h4>
+<br><br>
+<h2 class="title-2">Cara Connect ke Server Bot</h2>
+<h4 class="tutorial">
+ScreenShot: <a href="https://i.ibb.co/ZYHmL5h/Long-Screenshot-20211021-120906.png" target="_blank">Klik Disini</a>
+<br>
+Tutorial: <a href="https://f.top4top.io/m_2209cxa6n0.mp4" target="_blank">Klik Disini</a>
+</h4>
+<br>
+<h2 class="title-3">Informasi</h2>
+<h4 class="info">
+Chat dengan Bot: <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Klik Disini</a>
+<br>
+Join ke Grup Bot: <a href="https://chat.whatsapp.com/HDOZX7OoFYK1bTwftkY5Si" target="_blank">Klik Disini</a>
+<br>
+Donasi Sekarang: <a href="https://saweri.co/donate/RC047" target="_blank">Klik Disini</a>
+<br>
+Download AutoResponder (MOD): <a href="DOWNLOAD_LINK" target="_blank">Klik Disini</a>
+</h4>
 `.trim();
  div.innerHTML = html;
  window.setTimeout('runScript();', 1000);
+ });
+}
+
+function getBattery(callback) {
+
+navigator.getBattery()
+ .then(status => {
+ callback(status.level * 100);
+ });
 }
