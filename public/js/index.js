10d0c8b944db42a16897cbc898f3c734d332cb65
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-01-19 19:58:47 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,41 +6,61 @@ getBattery(battery => {
  var div = document.querySelector('div[class=content]');
  if (!div) return alert('Something went wrong :/');
  var date = new Date();
+ var ip = fetchJson('https://api.ipify.org/?format=json')
+ var quotes = fetchJson('https://kuhong-api.herokuapp.com/api/quotes?apikey=8RiU6O-yrLpgVep')
  var html = `
-<h2 class="title-1">Server Bot sedang Online!</h2>
-<br><br>
+<fieldset class="main">
+<legend style="text-align:center"><h2>Server sedang Online!</h2></legend>
 <h4>
 Time: ${[date.getHours(), date.getMinutes(), date.getSeconds()].join(':')}
 <br>
+IP Address: ${ip.ip}
+<br>
 Power: ${battery.level * 100}% ${battery.charging ? '(Charging)' : ''}
 <br>
-Cookie: ${navigator.cookieEnabled ? 'Enable' : 'Disable'}
+Cookie: ${navigator.cookieEnabled ? 'Enabled' : 'Disabled'}
 <br>
 Platform: ${navigator.platform}
 </h4>
-<br>
-<h2 class="title-2">Cara Connect ke Server Bot</h2>
-<h4 class="tutorial">
+</fieldset>
+<br><br>
+<fieldset class="tutorial">
+<legend style="text-align:center"><h2>Cara Connect ke Server</h2></legend>
+<h4>
 ScreenShot: <a href="https://i.ibb.co/ZYHmL5h/Long-Screenshot-20211021-120906.png" target="_blank">Klik Disini</a>
 <br>
 Tutorial: <a href="https://f.top4top.io/m_2209cxa6n0.mp4" target="_blank">Klik Disini</a>
-</h4>
 <br>
-<h2 class="title-3">Informasi</h2>
-<h4 class="info">
+AutoResponder (MOD): <a href="DOWNLOAD_LINK" target="_blank">Klik Disini</a>
+</h4>
+</fieldset>
+<br><br>
+<fieldset class="about">
+<legend style="text-align:center"><h2>Tentang</h2></legend>
+<h4>
 Chat dengan Bot: <a href="https://wa.me/62895337278647?text=!menu" target="_blank">Klik Disini</a>
 <br>
 Join ke Grup Bot: <a href="https://chat.whatsapp.com/HDOZX7OoFYK1bTwftkY5Si" target="_blank">Klik Disini</a>
 <br>
 Donasi Sekarang: <a href="https://saweri.co/donate/RC047" target="_blank">Klik Disini</a>
-<br>
-AutoResponder (MOD): <a href="DOWNLOAD_LINK" target="_blank">Klik Disini</a>
 </h4>
+</fieldset>
+<br><br><br>
+<marquee>${quotes.result}</marquee>
 `.trim();
  div.innerHTML = html;
  window.setTimeout('runScript();', 1000);
  });
 }
+
+function fetchJson(url, opts = { method: 'GET' }) {
+var xhr = new XMLHttpRequest();
+xhr.open(opts.method.toUpperCase(), url)
+xhr.send();
+var result = xhr.responseText ? xhr.responseText : xhr.response;
+  return JSON.parse(result);
+}
+
 
 function getBattery(callback) {
 navigator.getBattery()
