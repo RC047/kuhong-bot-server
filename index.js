c2ee65fd81c9e556d9fc71561c7951e0ef22fa11
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-02-19 18:19:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,7 @@ req.reply = (...message) => {
      if (message[i]) console.info(`${botName}:\n${message[i]}`)
      }
   }
-  var result = `{ "replies": [${message.reverse().map(v => `{ "message": "${util.format(v)}" }`).join(',')}] }`
-  fs.writeFileSync('./public/media/data.txt', result)
-    return res.status(200).json(JSON.parse(result));
+    return res.status(200).json({ status: res.statusCode || false, replies: message.reverse().map(v => new Object({ message: `${util.format(v)}` })) });
 }
 req.isWelcome = isWelcome == 'true' ? true : false;
 req.simiMode = simiMode == 'true' ? true : false;
