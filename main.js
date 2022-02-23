fead008643a84558ab57d798c73900e26adf2362
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-02-23 20:37:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ var loghandler = {
 }
 
 var listMenu = {
-	main: [
+    main: [
 'help',
 'menu',
 'start',
@@ -335,7 +335,7 @@ var listMenu = {
 
 try {
   if (m.isWelcome) {
-  	if (!senderMessage) return m.ignoreMessage()
+      if (!senderMessage) return m.ignoreMessage()
       var welcome = `Selamat ${salam}${senderName.startsWith('+') ? '\n' : ' '}*${senderName}*!\n\nSilahkan ketik *${prefix.test(senderMessage) ? usedPrefix : m.query.prefix ? m.query.prefix.slice(0, 1) : '!'}${pickRandom(listMenu.main)}* untuk memulai Bot ini.`
         return m.reply(welcome)
   } else if (m.simiMode) {
@@ -2895,11 +2895,11 @@ ${Math.floor(Math.random() * 50)} Zamrud
        $('div.wa-chat-body').each(function (c, d) {
            var link = $(d).find('a').attr('href')
            var subject = $(d).find('div.wa-chat-title-text').text().trim()
-           var hasil = { subject: subject, link: link }
+           var hasil = { subject: subject.split('. ')[1].split('*')[0], link: link }
            json.push(hasil)
        })
        if (!json[0].subject) return m.reply(loghandler.wait, `Pencarian Grup *${text}* tidak ditemukan!`)
-       var result = json.map(v => `Nama: ${v.subject}\nLink:\n${v.link}`).join('\n\n========================\n\n')
+       var result = json.map(v => `Judul: ${v.subject}\nLink:\n${v.link}`).join('\n\n========================\n\n')
          return m.reply(loghandler.wait, result)
       })
 
@@ -3211,10 +3211,11 @@ Kata sandi: superiorman_
          return eval(`m.reply('${result.slice(0, result.length - 3)}')`)
 
   } else if (/^kisahnabi$/i.test(command)) {
+  	 if (!text) return m.reply(loghandler.wait, loghandler.notText)
   	 var res = await fetch(`https://raw.githubusercontent.com/shansekai/My-SQL-Results/main/kisahnabi/${text}.json`)
        if (res.status !== 200) return m.reply(loghandler.wait, `Nama nabi *${text}* tidak dapat ditemukan!\n\nPastikan anda memasukan nama nabinya dengan benar`)
        var json = await res.json()
-       var result = `Kisah: ${json.name}\nTempat Kelahiran: ${json.tmp} (${json.thn_kelahiran})\nUsia: ${json.usia}\nThumb:\n${json.image_url}\n\n${json.description}`
+       var result = `Kisah: ${json.name}\nTempat Kelahiran: ${json.tmp} (${json.thn_kelahiran})\nUsia: ${json.usia}\nThumb:\n${json.image_url}\n\n\n${json.description}`
          return m.reply(loghandler.wait, result)
 
   } else return m.reply(loghandler.wait, loghandler.notCommand)
