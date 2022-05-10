efbf7bacc28bea5ccef937b0434708122dcde27f
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2022-05-10 08:02:30 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -24,10 +24,10 @@ window.setTimeout('runScripts();', 1000);
 function checkError() {
 var status = document.getElementById('status').textContent;
 if (status == 'Detecting...') {
-    document.querySelector('html').remove();
+    document.querySelector('body').remove();
     return document.write('<div style="text-align:center"><h1>Server is undergoing Maintenance</h1><br><h4>Report:<br><a href="https://wa.me/62895337278647" target="_blank">https://wa.me/62895337278647</a></h4></div>');
 } else if (status == 'Offline') {
-    document.querySelector('html').remove();
+    document.querySelector('body').remove();
     return document.write('<h1 style="text-align:center">Server is Offline!</h1>');
     }
 }
