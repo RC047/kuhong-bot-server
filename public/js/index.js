e5609248f2ff6ce9901d3869389a0ec7a5cc2aec
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2023-04-28 16:40:19 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 // Use Strict
 
 var ip = null
+var battery = null
 window.setTimeout('runScripts();', 1000);
 window.setTimeout('getFunFact();', 1000);
 window.setTimeout('checkError();', 5000);
@@ -9,23 +10,23 @@ window.setTimeout('checkError();', 5000);
 (async function() {
 try {
   ip = await (await fetch('https://api.ipify.org')).text()
+  battery = await navigator.getBattery()
 } catch (e) {
   ip = 'Not Located'
+  battery = { level: 0, charging: null }
 }
 })()
 
 function runScripts() {
-navigator.getBattery().then(status => {
-  var date = new Date();
-  document.getElementById('status').textContent = date.getHours() > 21 ? 'Offline' : 'Online';
-  document.getElementById('device').textContent = window.matchMedia('only screen and (max-width: 760px)').matches ? 'Mobiles' : 'Windows';
-  document.getElementById('times').textContent = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
-  document.getElementById('ip').textContent = ip;
-  document.getElementById('power').textContent = Math.floor(status.level * 100) + '%' + (status.charging ? ' (Charging)' : '');
-  document.getElementById('cookie').textContent = navigator.cookieEnabled ? 'Enabled' : 'Disabled';
-  document.getElementById('platform').textContent = navigator.platform;
-  document.getElementById('ping').textContent = Math.floor(Math.random() * 500) + 'ms';
-  });
+var date = new Date();
+document.getElementById('status').textContent = date.getHours() > 21 ? 'Offline' : 'Online';
+document.getElementById('device').textContent = window.matchMedia('only screen and (max-width: 760px)').matches ? 'Mobiles' : 'Windows';
+document.getElementById('times').textContent = new Array(date.getHours(), date.getMinutes(), date.getSeconds()).join(':');
+document.getElementById('ip').textContent = ip;
+document.getElementById('power').textContent = Math.floor(battery.level * 100) + '%' + (battery.charging ? ' (Charging)' : '');
+document.getElementById('cookie').textContent = navigator.cookieEnabled ? 'Enabled' : 'Disabled';
+document.getElementById('platform').textContent = navigator.platform;
+document.getElementById('ping').textContent = Math.floor(Math.random() * 500) + 'ms';
 window.setTimeout('runScripts();', 1000);
 }
 
@@ -36,7 +37,7 @@ if (status == 'Detecting...') {
     return document.write('<div style="text-align:center"><h1>Server is undergoing Maintenance</h1><br><h4>Report:<br><a href="https://wa.me/62895337278647" target="_blank">https://wa.me/62895337278647</a></h4></div>');
 } else if (status == 'Offline') {
     document.querySelector('html').remove();
-    return document.write('<h1 style="text-align:center">Server is Offline!</h1>');
+    return document.write('<h1 style="text-align:center">Server is Offline now!</h1>');
     }
 }
 
