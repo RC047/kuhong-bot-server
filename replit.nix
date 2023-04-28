58dbbc21c46a6fec17a737115b758296846b1df8
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2023-04-28 16:56:13 +0700

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{ pkgs }: {
+    deps = [
+      pkgs.nano
+      pkgs.nodejs-18_x
+      pkgs.nodePackages.typescript-language-server
+      pkgs.yarn
+      pkgs.libuuid
+      pkgs.replitPackages.jest
+    ];
+    env = {
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+    };
+}
